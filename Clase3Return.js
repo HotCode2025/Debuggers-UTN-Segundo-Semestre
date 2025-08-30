de71e5903cfda5d63cd1e1eff56395faf61042cf
Kevin-Mora96 <kevinjoelmoratal96@gmail.com>
2025-08-30 12:10:09 -0300

    Clase3Return
    Adjunto archivo de la clase 3 de JavaScript, Return

---
+++ Clase3Return.js
@@ -0,0 +1,10 @@
+miFuncion(8, 2);
+
+function miFuncion(a, b){
+    //console.log("Sumamos:"+ (a + b))
+    return a + b;
+}
+miFuncion(5, 4);
+
+let resultado = miFuncion(6, 7);
+console.log(resultado);
