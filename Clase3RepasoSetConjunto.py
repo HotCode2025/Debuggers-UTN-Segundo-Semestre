58c5b9da61220d85e64e6d78f906d6e4b277de69
Kevin-Mora96 <kevinjoelmoratal96@gmail.com>
2025-08-30 11:23:45 -0300

    3.1 Repaso de Set o Conjunto
    Adjunto el repaso de Set, primer video de la clase 3

---
+++ Clase3RepasoSetConjunto.py
@@ -0,0 +1,13 @@
+#Repaso de set o Conjunto
+# para definir un conjunto
+conjunto1 = set()
+conjunto2 = {'bye', }
+conjunto1.add(7)
+conjunto1.add('Hola')
+print(conjunto1)
+conjunto2.add('adiós')
+print(conjunto2)
+print(3 not in conjunto2) # Preguntamos si el número 3 NO está en el conjunto1
+
+# Como hacer la igualdad de dos conjuntos
+print(conjunto2 == conjunto1) #Nos devuelve como respuesta un booleano
