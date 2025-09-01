5918f0f795ab8b66f5952f8c34d5b80ac7c9c97c
Noelia Multari <multari.noelia.elisabet@gmail.com>
2025-09-01 01:19:02 -0300

    Agrego Ejercicio_1.4_Arreglos.js - Noelia Multari

---
+++ Ejercicio_1.4_Arreglos.js
@@ -0,0 +1,8 @@
+
+let autos = [6]
+console.log(autos); [ 'Ferrari', 'Volvo', 'BMw', 'Audi', 'Porche', 'Renault' ];
+
+// Cómo preguntar si es un Array o Arreglo
+console.log(Array.isArray(autos)); // Devuelve un booleano
+
+console.log(autos instanceof Array); // Preguntamos si la variable es una instancia de la clase Array
