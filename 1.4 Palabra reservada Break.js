f48ecca12d7313c6c8c3b0d7fbcb68d4b40f0885
Alberto Cantarelli <93921291+jacantarelli@users.noreply.github.com>
2025-09-06 18:18:20 -0300

    1.4 Palabra reservada Break

---
+++ 1.4 Palabra reservada Break.js
@@ -0,0 +1,9 @@
+// Palabra reservada: "break"
+for (let contador3 = 0; contador3 <= 10; contador3++) {
+    if (contador3 % 2 == 0) {
+        console.log(contador3);
+        break;
+    }
+}
+console.log("Termina el ciclo for al encontrar el primer numero par");
+
