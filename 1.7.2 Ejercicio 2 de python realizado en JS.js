30a49f944e84236e607c71475ee18cca56b1d63a
Alberto Cantarelli <93921291+jacantarelli@users.noreply.github.com>
2025-09-06 16:42:51 -0300

    1.7 Ejercicios clase 1 de Python realizados en JS

---
+++ 1.7.2 Ejercicio 2 de python realizado en JS.js
@@ -0,0 +1,8 @@
+// Ejercicio 2: Crear un rango de numeros del 2 al 6 e imprimirlo
+
+console.log('Crear un rango de numeros del 2 al 6 e imprimirlo');
+let numeros = [];
+for (i = 2; i <= 6; i++) {
+    numeros.push(i);
+}
+console.log(numeros);
---
+++ 1.7.3 Ejercicio 3 de python realizado en JS.js
@@ -0,0 +1,9 @@
+// Ejercicio 3: Crear un rango de 3 a 10 pero con incremento de 2 en 2
+
+console.log('Crear un rango de 3 a 10 pero con incremento de 2 en 2');
+let numeros1 = [];
+for (i = 3; i <= 10; i += 2) {
+    numeros1.push(i);
+}
+
+console.log(numeros1);
