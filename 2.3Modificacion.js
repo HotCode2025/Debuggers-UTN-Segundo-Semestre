aecbfaab42ea91706fe21a7c2e8b82c72713262e
Bruno Fiochi <brunofiochi19@gmail.com>
2025-09-20 18:57:56 -0300

    2.3 Modificacion de elementos

---
+++ 2.3Modificacion.js
@@ -0,0 +1,21 @@
+//Modificacion de elementos en una lista
+
+const autos =["Ferrari", "Renault","Bmw"];
+console.log(autos);
+
+
+//Modificamos un elemento del arreglo
+autos[1]="Volvo"
+console.log(autos);
+
+//Agregamos elementos al arreglo
+autos.push("Audi");
+console.log(autos);
+
+//Segunda forma
+autos[autos.length]="Porche";
+console.log(autos);
+
+//Tercera forma
+autos[7]="Renault";
+console.log(autos);
