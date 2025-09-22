35634c81efa2f56cb40f59297c3cd23b3a2b213f
EnzoNavarro36 <navarroenzo724@gmail.com>
2025-09-22 15:31:35 -0300

    Delete 6.8 Funciones: valores por Default en argumentos.py (reorganizacion)

--- 6.8 Funciones: valores por Default en argumentos.py
+++
@@ -1,9 +0,0 @@
-#6.8 Valores por default en Argumentos 
-
-def sumar2(a = 0, b = 0):  # Le damos un valor por default
-    return a + b
-
-resultado = sumar2()
-
-# print(f'Resultado de la suma: {resultado}')
-print(f'Resultado de la suma: {sumar2(22, 66)}')
