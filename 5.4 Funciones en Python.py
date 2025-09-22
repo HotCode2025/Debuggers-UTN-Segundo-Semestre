c013661fb31c925aedeca8ee5a1305910a7ada90
Enzo Navarro <navarroenzo724@gmail.com>
2025-09-22 15:51:01 -0300

    5.4 Funciones en Python
    Comenzamos a ver como funciona una FUNCION en Python, viendo que podemos llamarla en cualquier momento y N cantidad de veces, pero siempre debe llamarse despues de ser definida.

---
+++ 5.4 Funciones en Python.py
@@ -0,0 +1,10 @@
+#una funcion en python es una opcion que puede ser llamada indefinidamente
+#Definimos una funcion
+#no se puede llamar la funcion antes de definirla
+def mi_funcion(): #para identificar la funcion utilizamos parentesis
+    print("Saludos a todos los que me conocen")
+    print("Espero esten teniendo un buen dia")
+    print("Soy Enzo Navarro") #todo lo que este dentro de esta tabulacion/identacion, es parte de la funcion
+
+mi_funcion() # llamamos a la funcion definida arriba
+mi_funcion() #se puede llamar una funcion N cantidad de veces
