1097a17458fdcd0ade770ec892bf4897d5cdef0b
Celeste Dominguez <dominguezmceleste@gmail.com>
2025-09-23 09:35:17 -0300

    Delete 5.5 El uso de prototype.js

--- 5.5 El uso de prototype.js
+++
@@ -1,7 +0,0 @@
-// Uso de prototype
-Persona3.prototype.telefono = '2618383832';
-console.log(padre);
-console.log(madre.telefono);
-madre.telefono = '5492618383832';
-console.log(madre.telefono);
-
