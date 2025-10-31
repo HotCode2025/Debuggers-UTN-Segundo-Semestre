962efc800d9683639c3c6e7dfa7c047f6b87ea21
Bruno Fiochi <brunofiochi19@gmail.com>
2025-10-31 19:27:51 -0300

    14.5 Constantes
     Test Constante

---
+++ 14.5 Constantes.py
@@ -0,0 +1,7 @@
+from Clase10_B.Constantes import *
+from Clase10_B.Constantes import Matematica as Mate
+print(MI_CONSTANTE)
+
+#MI_CONSTANTE="Nuevo valor de la constante"  # No se puede cambiar el valor de una constante
+#print(MI_CONSTANTE)
+print(Mate)
